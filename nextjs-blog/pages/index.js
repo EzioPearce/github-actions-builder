909c588ceddbe00f4448d1c829f6dedf795dc5b0
krishna <krishna.vaddamani@gmail.com>
2023-09-29 17:17:13 +0530

    fix: update text

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Bonk <code>pages/index.js</code>
+          Bonk <code>Pls work/index.js</code>
         </p>
 
         <div className={styles.grid}>
